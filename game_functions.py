d436beda0f0729c733ab050f5f12bc7a6213d660
RacheliShayzada <054racheli@gmail.com>
2025-07-09 21:49:58 +0300

    update get_guess_from_user function to support in lower case

--- game_functions.py
+++ game_functions.py
@@ -1,4 +1,4 @@
 def get_guess_from_user():
-    """Ask the player to guess a letter and return the input."""
+    """Ask the player to guess a letter and return it in lowercase."""
     guess = input("Guess a letter: ")
-    return guess
+    return guess.lower()
