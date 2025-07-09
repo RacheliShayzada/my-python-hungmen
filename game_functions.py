00da193e90657846a82d01ff3577ab875bacfad7
RacheliShayzada <054racheli@gmail.com>
2025-07-09 21:17:12 +0300

    adding get_guess_from_user function

---
+++ game_functions.py
@@ -0,0 +1,4 @@
+def get_guess_from_user():
+    """Ask the player to guess a letter and return the input."""
+    guess = input("Guess a letter: ")
+    return guess
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from consts import HANGMAN_ASCII_ART, MAX_TRIES
+from game_functions import get_guess_from_user
+
 
 def main():
     print(HANGMAN_ASCII_ART + str(MAX_TRIES))
+    
+    guess = get_guess_from_user()
+    print(guess)
 
 
 if __name__ == "__main__":
